cf019d7ecc97dd9a88fd6a490e435f77b4846713
Razum <razumyeyenko@gmail.com>
2016-02-08 08:45:25 +0200

    project page

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -5,7 +5,9 @@ import "../../sass/app.scss";
 import Router from './router.js';
 
 "use strict";
+
 console.info(new Router());
+var router = new Router({root: "/", mode: "history"})
 /*
  if (__DEV__) {
  console.log("DEV");
@@ -15,7 +17,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var wrapper = document.getElementById("wrapper");
     var folding_panel = document.getElementById("folding-panel");
     var fold_content = document.getElementById("fold-content");
-    var flipper = document.getElementById("flipper");
+    var overlay = document.getElementById("page-overlay");
+    var page_content = document.getElementById("page-content");
 
     var current_page = "";
 
@@ -26,12 +29,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         while (target.tagName != "BODY") {
             if (target.tagName === "A" && target.classList.contains("page-item-link")) {
-                wrapper.classList.add('fold-is-open');
+
+                var mq = window.matchMedia( "(min-width: 1024px)" );
                 href = target.getAttribute('href');
                 current_page = href.slice(0, -5);
-                folding_panel.classList.add('is-open', current_page);
-                openItemInfo(href);
-                //flipper.classList.add("active");
+                if (mq.matches) {
+                    document.querySelector("body").classList.add("page-is-shown");
+                    openItemInfo(href, "page-content");
+                    page_content.classList.add(current_page);
+                } else {
+                    wrapper.classList.add('fold-is-open');
+                    folding_panel.classList.add('is-open', current_page);
+                    openItemInfo(href, "fold-content");
+                }
+
 
 
                 break;
@@ -50,17 +61,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
         folding_panel.classList.remove('is-open', current_page);
     }, false);
 
-    function openItemInfo(url) {
+    function openItemInfo(url, id) {
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
         xhr.send();
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
-                fold_content.innerHTML = xhr.responseText;
+                document.getElementById(id).innerHTML = xhr.responseText;
             }
 
         }
     }
+
+    overlay.addEventListener("click", function (evt) {
+        var target = evt.target;
+        if (target.tagName === "DIV" && target.classList.contains("page-content-overlay")) {
+            document.querySelector("body").classList.remove("page-is-shown");
+            page_content.classList.remove(current_page);
+        }
+
+    }, false);
 
 
 
@@ -82,6 +102,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 target = target.parentNode;
             }
         }
-    }, false)
+    }, false);
+    page_content.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        var target = evt.target;
+        while (target.tagName != "BODY") {
+            if (target.tagName === "LI" && target.classList.contains("skill-category")) {
+                [].forEach.call(document.querySelectorAll('.skill-category'), function (elm) {
+                    elm.classList.remove("active")
+                });
+                target.classList.add("active");
+
+                var category = target.dataset.category;
+                document.getElementById("skill-list").setAttribute("class", "skill-list " + category);
+
+                break;
+            } else {
+                target = target.parentNode;
+            }
+        }
+    }, false);
 
 });
--- src/js/app/router.js
+++ src/js/app/router.js
@@ -3,7 +3,23 @@
  */
 console.log("ROUTER");
 export default class Router {
-    constructor() {
+    constructor({root, mode = this._getDefaultMode(), } = {}) {
         console.info("constructor");
+        this.root = root ? '/' + this._clearSlashes(root) + '/' : '/';
+
+        console.log(111, ...arguments, arguments, 222, mode);
     }
+    _getFragment () {
+        var fragment = '';
+    }
+
+    _getDefaultMode () {
+        return !!(history.pushState) ? 'history' : 'hash'
+    }
+
+    _clearSlashes (path) {
+        return path.toString().replace(/\/$/, '').replace(/^\//, '');
+    }
+
+    //const routes = [];
 }
